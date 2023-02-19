93d7494cacbdec8fddea0b8701c82cf68d8ede19
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2023-02-18 23:18:36 -0500

    creando inyección de dependencias

---
+++ SistemaVenta.DAL/Implementacion/GenericRepository.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 
+using SistemaVenta.DAL.DBContext;
+using SistemaVenta.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace SistemaVenta.DAL.Implementacion
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        // Obtenemos el contexto 
+        private readonly DbventaContext _dbContext;
+
+        // Constructor para inicialización del contexto
+        public GenericRepository(DbventaContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        
+        // Evento para obtener
+        public async Task<TEntity> Obtener(Expression<Func<TEntity, bool>> filtro)
+        {
+            try
+            {
+                // Accedemos al contexto de la entidad a procesar y recuperamos el primero de las coincidencias
+                TEntity entidad = await _dbContext.Set<TEntity>().FirstOrDefaultAsync(filtro);
+
+                // Retornamos
+                return entidad;
+            } 
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        // 
+        public async Task<TEntity> Crear(TEntity entidad)
+        {
+            try
+            {
+                // Creamos un nuevo registro de la entidad a procesar
+               _dbContext.Set<TEntity>().Add(entidad);
+
+                // Guardamos
+                await _dbContext.SaveChangesAsync();
+
+                // Retornamos
+                return entidad;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        // 
+        public async Task<bool> Editar(TEntity entidad)
+        {
+            try
+            {
+                // Actualizamos contexto de la entidad
+                _dbContext.Update(entidad);
+
+                // Guardamos
+                await _dbContext.SaveChangesAsync();
+
+                // Retornamos
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        // 
+        public async Task<bool> Eliminar(TEntity entidad)
+        {
+            try
+            {
+                // Actualizamos contexto de la entidad
+                _dbContext.Remove(entidad);
+
+                // Guardamos
+                await _dbContext.SaveChangesAsync();
+
+                // Retornamos
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        // 
+        public async Task<IQueryable<TEntity>> Consultar(Expression<Func<TEntity, bool>> filtro = null)
+        {
+            // Buscamos por medio de filtro, en caso que no existan datos a filtrar trae lista por default
+            IQueryable<TEntity> queryEntidad = filtro == null ? _dbContext.Set<TEntity>() : _dbContext.Set<TEntity>().Where(filtro);
+
+            // Retornamos
+            return queryEntidad;
+        }
+
+    }
+}
---
+++ SistemaVenta.DAL/Implementacion/VentaRepository.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 
+using Microsoft.EntityFrameworkCore;
+using SistemaVenta.DAL.DBContext;
+using SistemaVenta.DAL.Interfaces;
+using SistemaVenta.Entity;
+
+namespace SistemaVenta.DAL.Implementacion
+{
+    public class VentaRepository : GenericRepository<Venta>, IVentaRepository
+    {
+        private readonly DbventaContext _dbContext;
+        public VentaRepository(DbventaContext dbContext): base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<Venta> Registrar(Venta entidad)
+        {
+            Venta ventaGenerada = new Venta();
+
+            using(var transaction = _dbContext.Database.BeginTransaction())
+            {
+                try
+                {
+                    foreach(DetalleVenta dv in entidad.DetalleVenta)
+                    {
+                        // Buscamos el producto a iterar para restar al stock
+                        Producto producto_encontrado = _dbContext.Productos.Where(p => p.IdProducto == dv.IdProducto).First();
+
+                        // Restamos stock de producto
+                        producto_encontrado.Stock = producto_encontrado.Stock - dv.Cantidad;
+
+                        // Actualizamos
+                        _dbContext.Productos.Update(producto_encontrado);
+                    }
+
+                    // Esperamos evento...
+                    await _dbContext.SaveChangesAsync();
+
+                    // Numero correlativo...
+                    NumeroCorrelativo correlativo = _dbContext.NumeroCorrelativos.Where(n => n.Gestion == "venta").First();
+
+                    correlativo.UltimoNumero = correlativo.UltimoNumero + 1;
+                    correlativo.FechaActualizacion = DateTime.Now;
+
+                    // Actualizamos numero correlativo...
+                    _dbContext.NumeroCorrelativos.Update(correlativo);
+
+                    // Esperamos evento...
+                    await _dbContext.SaveChangesAsync();
+
+                    // Generamos string de ceros
+                    string ceros = string.Concat(Enumerable.Repeat("0", correlativo.CantidadDigitos.Value));
+
+                    // Generamos numero de venta
+                    string numeroVenta = ceros + correlativo.UltimoNumero.ToString();
+
+                    // 
+                    numeroVenta = numeroVenta.Substring(numeroVenta.Length - correlativo.CantidadDigitos.Value, correlativo.CantidadDigitos.Value);
+
+                    // 
+                    entidad.NumeroVenta = numeroVenta;
+
+                    // Agregamos registro a modelo de datos
+                    await _dbContext.Venta.AddAsync(entidad);
+
+                    // Guardamos...
+                    await _dbContext.SaveChangesAsync();
+
+                    // 
+                    ventaGenerada = entidad;
+
+                    // Evento que se encarga de confirmar la creación de la transacción de manera exitosa.
+                    transaction.Commit();
+
+                }
+                catch(Exception ex)
+                {
+                    // En caso que suceda algún error en la transacción, devolvemos el error
+                    transaction.Rollback();
+
+                    throw ex;
+                }
+
+                // Retornamos...
+                return ventaGenerada;
+            }
+        }
+
+        public async Task<List<DetalleVenta>> Reporte(DateTime FechaInicio, DateTime FechaFin)
+        {
+            List<DetalleVenta> listaResumen = await _dbContext.DetalleVenta
+                .Include(v => v.IdVentaNavigation)
+                .ThenInclude(u => u.IdUsuarioNavigation)
+                .Include(v => v.IdVentaNavigation)
+                .ThenInclude(tdv => tdv.IdTipoDocumentoVentaNavigation)
+                .Where(dv => dv.IdVentaNavigation.FechaRegistro.Value.Date >= FechaInicio.Date &&
+                dv.IdVentaNavigation.FechaRegistro.Value.Date <= FechaFin.Date).ToListAsync();
+
+            // Retornamos...
+            return listaResumen;
+        }
+    }
+}
