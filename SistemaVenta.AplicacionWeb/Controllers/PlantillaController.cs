32170e923b4b9bc319c9a434df60644e82347d71
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2023-02-19 09:41:13 -0500

    creando utilidades para generar contraseña y encriptar contraseña - integrando genericResponse

---
+++ SistemaVenta.AplicacionWeb/Controllers/PlantillaController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaVenta.AplicacionWeb.Controllers
+{
+    public class PlantillaController : Controller
+    {
+        // 
+        public IActionResult EnviarClave(string correo, string clave)
+        {
+            ViewData["Correo"] = correo;
+            ViewData["Clave"] = clave;
+            ViewData["Url"] = $"{this.Request.Scheme}://{this.Request.Host}";
+
+            // Retornamos vista y enviamos parametros...
+            return View();
+        }
+
+        // 
+        public IActionResult RestablecerClave(string clave)
+        {
+            ViewData["Clave"] = clave;
+
+            return View();
+        }
+    }
+}
